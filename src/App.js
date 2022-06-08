7d118d3cf1991c8dd2c04657877c8e5cd092c3ed
Sergey Shkret <Freedom170691.gmail.com>
2022-06-09 00:02:46 +0300

    #1 Add style and low code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {useState} from "react";
-import {nanoid} from "nanoid";
 import meatImage from './assets/meat.jpg';
 import cheeseImage from './assets/cheese.png';
 import saladImage from './assets/salat.png';
@@ -9,17 +8,66 @@ import './Burger.css';
 
 
 const INGREDIENTS = [
-  {name: 'Meat', price: 50, image: meatImage},
-  {name: 'Cheese', price: 20, image: cheeseImage},
-  {name: 'Salad', price: 5, image: saladImage},
-  {name: 'Bacon', price: 30, image: baconImage},
+  {name: 'Meat', price: 50, image: meatImage, index: 0},
+  {name: 'Cheese', price: 20, image: cheeseImage, index: 1},
+  {name: 'Salad', price: 5, image: saladImage, index: 2},
+  {name: 'Bacon', price: 30, image: baconImage, index: 3},
 ]
 function App() {
+
+    const [ingredients, setIngredients] = useState([
+        // {name:'Meat', count: 0},
+        // {name:'Cheese', count: 0},
+        // {name:'Salad', count: 0},
+        // {name:'Bacon', count: 0},
+    ])
+
+    const onClick = index => {
+        const ingrid = INGREDIENTS.map(ing => {
+            if (ing.index === index) {
+                ingredients.push({name: ing.name, count: 0})
+                // console.log(ing.index)
+                return ing;
+            }
+            // return ing
+        });
+
+        const ingredientsCopy = ingredients.map(ing => {
+            if (INGREDIENTS[index].name === ing.name) {
+                return {
+                    ...ing,
+                    count: ing.count + 1,
+                }
+                console.log(INGREDIENTS[index].name)
+            }
+        })
+
+
+        // setIngredients(ingredientsCopy);
+        console.log(ingredientsCopy)
+    }
 
   return (
     <div className="App">
         <div className="BoxIngredients">
           <h3>Ingredients</h3>
+            {
+                INGREDIENTS.map(ingredient => {
+                    return (
+                        <div className="Item"
+                            key={ingredient.index}
+                        >
+                            <div className="MeatIngredient" onClick={() => onClick(ingredient.index)}>
+                                <img width="80px" height="80px" src={ingredient.image} alt=""/>
+                            </div>
+                            <span>{ingredient.name}</span>
+                            <span className="AmountIngredient"> x </span>
+                            <button type="button">Delete</button>
+                        </div>
+                    )
+                })
+            }
+
         </div>
         <div className="BoxBurger">
             <h3>Burger</h3>
